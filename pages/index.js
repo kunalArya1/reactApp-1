18c77b32c348f345372f3869c0c1382a12b252aa
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-06-12 18:24:55 +0530

    class-1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 
-export default function Home() {
-  return <div>Hello World</div>
+export default function Home() {    // function component 
+
+  //plane javaScript
+  // for writing js in jsx we use --> {}
+  // we cannot use our variable in reat 
+  // we have to use react variable i.e useState
+  // useState -> react -> virtual dom -> real dom
+  const a = new Date().toLocaleTimeString();
+
+  setInterval(() => {
+    
+    
+  }, 1000);
+
+
+
+  return   <h1>{a}</h1>;
+  // we return always html 
     
 }
